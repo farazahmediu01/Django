4881d61094e48b932b82cd880ea0b3fc4e9d92c5
Faraz Ahmed <farazahmediu01@gmail.com>
2023-12-05 11:29:58 +0000

    list view, detail view

--- project_01/crud_app/urls.py
+++ project_01/crud_app/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from crud_app import views
+
 
 urlpatterns = [
+    path("<str:id>/detail", views.detail_view, name="detail_view"),
+    path("", views.home, name="home"),
 ]
--- project_01/crud_app/views.py
+++ project_01/crud_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from crud_app.models import Post
+
 
 # Create your views here.
+def home(request):
+    post_list = Post.objects.all() # Select all from Post
+    
+    return render(request, "home.html", {
+        "post_list": post_list,
+    })
+
+def detail_view(request, id):
+    post = Post.objects.get(pk=id) #Select post that matches specific id
+    
+    return render(request, "detail.html",{
+        "post": post,
+    })
